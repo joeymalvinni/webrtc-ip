fee20f1a1f3c0b6aadb2bc38dbc19457c67b27fe
Joey <malvinniguitar@gmail.com>
2021-01-22 11:28:24 -0800

    v1.3.0: Refactoring of structure, updated src/index.js to remove getIpArray duplicate getFirstIp, which now calls getIpArray instead.

--- browser/webrtc.js
+++ browser/webrtc.js
@@ -13,17 +13,13 @@
             useWebKit = !!win.webkitRTCPeerConnection;
         }
     
-        var mediaConstraints = getConstraints()
-    
-        var servers = getServers()
-    
-        var pc = new RTCPeerConnection(servers, mediaConstraints);
+        var pc = new RTCPeerConnection(getServers(), getConstraints());
     
         function handleCandidate(candidate){
             var regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
             if(regex.exec(candidate) !== null){
                 var ip_addr = regex.exec(candidate)[0];
-              if(validateIp(ip_addr)) finalArray.push(ip_addr)
+                if(validateIp(ip_addr)) finalArray.push(ip_addr)
             }  
         }
     
@@ -31,7 +27,7 @@
             if(ice.candidate) handleCandidate(ice.candidate.candidate);
         };
     
-        pc.createDataChannel("ip");
+        pc.createDataChannel("fake_data_channel");
     
         pc.createOffer(function(result){
             pc.setLocalDescription(result, function(){}, function(){});
@@ -47,79 +43,41 @@
             let truncatedArray = removeDuplicates(finalArray)
             callback(truncatedArray)
         }, 100);
+    
+        function getServers(){
+            return { 
+                iceServers: [{ 
+                    urls: ["stun:stun.l.google.com:19302?transport=udp"] 
+                }] 
+            };
+        }
+        
+        function getConstraints(){
+            return {
+                optional: [{RtpDataChannels: true}]
+            };
+        }
+        
+        function getPeerConn(){
+            return window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        }
+        
+        function validateIp(ipaddress) {  
+          if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+            return (true)  
+          }  
+          return (false)  
+        }
+        
+        function removeDuplicates(array){
+          return [...new Set(array)];
+        }
     }
     
     function getFirstIp(callback){
-        let ips = []
-        var RTCPeerConnection = getPeerConn()
-        var useWebKit = !!window.webkitRTCPeerConnection;
-    
-        if(!RTCPeerConnection){
-            var win = iframe.contentWindow;
-            RTCPeerConnection = win.RTCPeerConnection
-                || win.mozRTCPeerConnection
-                || win.webkitRTCPeerConnection;
-            useWebKit = !!win.webkitRTCPeerConnection;
-        }
-    
-        var mediaConstraints = getConstraints()
-    
-        var servers = getServers()
-    
-        var pc = new RTCPeerConnection(servers, mediaConstraints);
-    
-        function handleCandidate(candidate){
-            var regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-            if(regex.exec(candidate) !== null){
-                var ip_addr = regex.exec(candidate)[0];
-              if(validateIp(ip_addr)) ips.push(ip_addr);
-            }
-        }
-    
-        pc.onicecandidate = function(ice){
-            if(ice.candidate) handleCandidate(ice.candidate.candidate);
-        };
-    
-        pc.createDataChannel("ip");
-    
-        pc.createOffer(function(result){
-            pc.setLocalDescription(result, function(){}, function(){});
-        }, function(){});
-    
-        setTimeout(function(){
-            var lines = pc.localDescription.sdp.split('\n');
-    
-            lines.forEach(function(line){
-                if(line.indexOf('a=candidate:') === 0)
-                    handleCandidate(line);
-            });
-            callback(ips[0])
-        }, 100);
-    }
-    
-    function getServers(){
-        return { iceServers: [{ urls: ["stun:stun.l.google.com:19302?transport=udp"] }] };
-    }
-    
-    function getConstraints(){
-        return {
-              optional: [{RtpDataChannels: true}]
-        };
-    }
-    
-    function getPeerConn(){
-        return window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    }
-    
-    function validateIp(ipaddress) {  
-      if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
-        return (true)  
-      }  
-      return (false)  
-    }
-    
-    function removeDuplicates(array){
-      return [...new Set(array)];
+        getIpArray(function(ips){
+            callback(ips[0] || null)
+        })       
     }
     
     function getIpTypes(callback){
@@ -128,13 +86,13 @@
             ips.forEach(ip=>{
                 if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/)) {
                     finalIpArray.push({ ip: ip, type: 'local'})
-                } else if (ip.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/)){
+                } else if (ip.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/)) {
                     finalIpArray.push({ ip: ip, type: 'IPv6'})
                 } else {
                     finalIpArray.push({ ip: ip, type: 'public'})
                 }
-          })
-          callback(finalIpArray)
+            })
+            callback(finalIpArray)
         })       
     }
 },{}]},{},[1]);
