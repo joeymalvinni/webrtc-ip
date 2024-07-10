3c8497ae4e378ea5bc92f043949811377e494d54
= <joeymalvinni@gmail.com>
2024-07-09 18:44:45 -0700

    a

--- example/components/webrtc-ip.ts
+++ example/components/webrtc-ip.ts
@@ -1,5 +1,3 @@
-// import { parse_ice_candidate, minimal_parse, IceCandidate, IPH } from "./candidate";
-
 export async function getIP(stun: string = "stun:stun.l.google.com:19302"): Promise<string> {
   if (typeof window === 'undefined') {
     return "";
@@ -12,7 +10,7 @@ export async function getIP(stun: string = "stun:stun.l.google.com:19302"): Prom
   const p = new RTCPeerConnection(config);
 
   return new Promise<string>((resolve, reject) => {
-    p.onicecandidate = (event) => {
+   p.onicecandidate = (event) => {
       if (event.candidate && event.candidate.candidate) {
         let split = event.candidate.candidate.split(" ");
 
